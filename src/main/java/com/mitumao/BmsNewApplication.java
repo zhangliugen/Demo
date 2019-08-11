e0fcb23ba3c3632535aaa185ea00aa0996fdc16a
zhangliugen <13063419587@163.com>
2019-08-11 12:33:02 +0800

    first commit

---
+++ src/main/java/com/mitumao/BmsNewApplication.java
@@ -0,0 +1,13 @@
+package com.mitumao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BmsNewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BmsNewApplication.class, args);
+	}
+
+}
